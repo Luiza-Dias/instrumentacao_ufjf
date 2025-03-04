c82f502669004947a033210aa5fa5888f945994a
Luiza-Dias <66181468+Luiza-Dias@users.noreply.github.com>
2025-03-04 01:11:03 -0300

    adicionando calculo potencia e energia

--- leitura_tensao_corrente/leitura_tensao_corrente.ino
+++ leitura_tensao_corrente/leitura_tensao_corrente.ino
@@ -3,14 +3,32 @@
 Leitura_sensor leitura_tensao = Leitura_sensor(A1, 0.0053);  
 Leitura_sensor leitura_corrente = Leitura_sensor(A0, 0.066);  
 
+float energia;
+uint32_t t_0;
+
 void setup() {
   Serial.begin(115200);
+  energia = 0;
+  t_0 = millis();
 }
 
 void loop() {
-  Serial.print("Valor de tensao lido (RMS): ");
-  Serial.println(leitura_tensao.retorna_rms());
+  float tensao = leitura_tensao.retorna_rms();
+  float corrente = leitura_corrente.retorna_rms();
+  float potencia = tensao * corrente;
+  energia = energia + (potencia * (millis() - t_0)/3600000);
+  t_0 = millis();
 
-  Serial.print("Valor de corrente lido (RMS): ");
-  Serial.println(leitura_corrente.retorna_rms());
+  Serial.print("Valor de tensao lido (RMS) [V]: ");
+  Serial.println(tensao);
+
+  Serial.print("Valor de corrente lido (RMS) [A]: ");
+  Serial.println(corrente);
+
+  Serial.print("Valor de potencia calculado (RMS) [W]: ");
+  Serial.println(potencia);
+
+  Serial.print("Valor de energia calculado (RMS) [KWh]: ");
+  Serial.println(energia);
 }
+//TO-DO Inserir display para mostrar os valores sem depender do terminal
